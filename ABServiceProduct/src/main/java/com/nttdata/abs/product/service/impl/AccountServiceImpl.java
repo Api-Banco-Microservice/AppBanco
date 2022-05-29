786b640683508fa5539f15b534417ad65ebdcce0
Megex04 <miantolalfa04@gmail.com>
2022-05-28 20:43:16 -0500

    Adding/Change AccountService and AccountServiceImpl

--- ABServiceProduct/src/main/java/com/nttdata/abs/product/service/impl/AccountServiceImpl.java
+++ ABServiceProduct/src/main/java/com/nttdata/abs/product/service/impl/AccountServiceImpl.java
@@ -1,8 +1,11 @@
 package com.nttdata.abs.product.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import com.nttdata.abs.product.entity.Account;
@@ -10,7 +13,7 @@ import com.nttdata.abs.product.repository.AccountRepository;
 import com.nttdata.abs.product.service.AccountService;
 
 @Service
-public class AccountServiceImpl implements AccountService {
+public abstract class AccountServiceImpl implements AccountService {
 
 	@Autowired
 	private AccountRepository repository;
@@ -18,31 +21,38 @@ public class AccountServiceImpl implements AccountService {
 	@Override
 	public List<Account> findAll() {
 		// TODO Auto-generated method stub
+		List<Account> returnList = new ArrayList<>();
+		getAccount().findAll().forEach(obj -> returnList.add(obj));
 		return null;
 	}
 
 	@Override
 	public Account findById(String id) {
 		// TODO Auto-generated method stub
+		Optional<Account> obj = getAccount().findById(id);
+		if(obj.isPresent()) {
+			return obj.get();
+		}
 		return null;
 	}
 
 	@Override
 	public Account createClient(Account account) {
 		// TODO Auto-generated method stub
-		return null;
+		return getAccount().save(account);
 	}
 
 	@Override
 	public Account updateClient(Account account) {
 		// TODO Auto-generated method stub
-		return null;
+		return getAccount().save(account);
 	}
 
 	@Override
 	public void deleteClient(String id) {
 		// TODO Auto-generated method stub
-		
+		getAccount().deleteById(id);
 	}
+	public abstract CrudRepository<Account, String> getAccount();
 
 }
