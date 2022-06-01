82f987e591f611554b69d09803653a19dd3656c5
Karc <kevinrojascobenas@gmail.com>
2022-05-31 22:13:44 -0500

    Add Service Eureka

--- ABServiceCustomer/src/main/java/com/nttdata/abs/customer/AbServiceCustomerApplication.java
+++ ABServiceCustomer/src/main/java/com/nttdata/abs/customer/AbServiceCustomerApplication.java
@@ -2,16 +2,18 @@ package com.nttdata.abs.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@EnableEurekaClient
 @SpringBootApplication
 public class AbServiceCustomerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbServiceCustomerApplication.class, args);
-		log.info("---> Service Customer complete!!");
+		log.info("Customer service running.");
 	}
 
 }
